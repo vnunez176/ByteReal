2015534913cf86883e2bf060cf8812bdd0e438f0
vnunez176 <victoria9339@gmail.com>
2024-10-09 15:07:19 -0500

    Initial Commit

---
+++ ByteReal/ByteRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByteRealApp.swift
+//  ByteReal
+//
+//  Created by Victoria Nunez on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ByteRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ByteRealTests/ByteRealTests.swift
@@ -0,0 +1,17 @@
+//
+//  ByteRealTests.swift
+//  ByteRealTests
+//
+//  Created by Victoria Nunez on 10/9/24.
+//
+
+import Testing
+@testable import ByteReal
+
+struct ByteRealTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
